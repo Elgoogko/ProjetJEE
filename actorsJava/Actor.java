aa0903d4dda69c9f57359cb7c293b52bec36b131
Lyx-Orl <lyxorl@proton.me>
2025-10-20 10:25:57 +0200

    update actor

--- actorsJava/Actor.java
+++ actorsJava/Actor.java
@@ -1,5 +1,76 @@
+/**
+ * 
+ * Interface Actor
+ * 
+ * Setup id to allow interaction with a micro service wich don't know a speficif actor
+ * Example : Client and Film are in different micro service
+ * So if we send an message between them with actor structur that can be interpreted
+ * So id allow to just take id get more information or specific information later
+ * 
+ */
 interface Actor {
-    public void send(String message, Actor actor);
-    public void receive(String message, Actor actor);
-    public String getName();
+
+    /**
+     * 
+     * 
+     * static idList; structur of id with subactor if there is subactor
+     * int id; id of the actor
+     * 
+     */
+
+    
+    /**
+     * Send a message to a specific actor
+     * 
+     * @param message 
+     * @param actor 
+     */
+    public void sendWithActor(String message, Actor actor);
+
+    /**
+     * Receive message from a specific actor
+     * 
+     * @param message
+     * @param actor
+     */
+    public void receiveWithActor(String message, Actor actor);
+
+    /**
+     * 
+     * Send message to an actor with his id
+     * 
+     * @param message
+     * @param id
+     */
+    public void sendWithId(String message, int id);
+
+    /**
+     * 
+     * Receive a message from an actor with given id
+     * 
+     * @param message
+     * @param id
+     */
+    public void receiveWithId(String message, int id);
+
+    /**
+     * Creattion of a subACtor
+     * At ovverride with the actor structur in actor
+     * write a method to generate new id
+     */
+    public void createNewActor();
+
+    /**
+     * Check if there is an actor who have this id
+     * 
+     * @param id which want to know if there is an actor with it
+     * @return true if there is an actor who have id  in this obkectb type
+     */
+    public boolean thisIdExist(int id);
+
+    /**
+     * getter of id
+     * @return id of the actor
+     */
+    public int getId();
 }
