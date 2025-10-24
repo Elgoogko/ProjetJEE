cc3b81e953beffe2fb936b4ea630b44733b11dc8
Elgoogko <boicosb@gmail.com>
2025-10-24 13:24:04 +0200

    update : rename service A -> msfilm

--- springBoot/MSFilm/src/main/java/com/msfilm/msfilm/MSFilmApplication.java
+++ springBoot/MSFilm/src/main/java/com/msfilm/msfilm/MSFilmApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceaApplication {
+public class MSFilmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceaApplication.class, args);
+		SpringApplication.run(MSFilmApplication.class, args);
 	}
 
 }
