f2d7b0a0479cd12b048f394ab424736ecd31174b
Happy0503 <happy0503.pro@gmail.com>
2025-11-17 20:56:46 +0100

    Added more on SecurityConfig.java, added test messages for adminDashboard.html and home.html + added custom error.html page (unfinished)

--- springBoot/MSClient/src/main/resources/Config/SecurityConfig.java
+++ springBoot/MSClient/src/main/resources/Config/SecurityConfig.java
@@ -24,6 +24,28 @@ import java.util.Arrays;
 @Configuration
 public class SecurityConfig{
 
+    @Autowired
+    private Environment environment; //Permet d'accéder à la propriété liée aux profils actifs
+
+    /**
+     * Vérifie si un profil est actif dans les propriétés de l'application
+     * @param profile - le nom du profil
+     * @return - si un profil est actif
+     */
+    private boolean isProfileActive(String profile){
+        //final String[] profiles = environment.getActiveProfiles();
+        //return Arrays.asList(profiles).contains(profile);
+        return Arrays.asList(environment.getActiveProfiles()).contains(profile);
+    }
+
+    /**
+     * Définition de la hiérarchie des rôles utilisateur
+     * Les rôles supérieurs héritent des permissions des rôles inférieurs
+     */
+    @Bean
+    public RoleHierarchy roleHierarchy(){
+        return RoleHierarchyImpl.fromHierarchy("ROLE_DEV > ROLE_ADMIN > ROLE_USER");
+    }
 
     /**
      * Définition de :
@@ -35,7 +57,42 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomLoginSuccessHandler loginSuccessHandler,
                                                    UserExistenceFilter userExistenceFilter) throws Exception {
-        //
+        http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/auth/**","/h2-console/**")) // Désactivation de la protection CSRF (nécessaire pour H2-Console)
+
+                .addFilterBefore(userExistenceFilter, UsernamePasswordAuthenticationFilter.class) // Filtre la connexion des sessions en mémoire de comptes qui n'existent plus
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/","/login","/register").permitAll(); // Pages publiques (sans connexion)
+                    auth.requestMatchers("/admin/**").hasRole("ADMIN"); // Pages dans /admin/ restreintes aux ADMIN et supérieurs
+
+                    auth.anyRequest().authenticated(); // Toute les autres requêtes nécessitent authentification
+                })
+                .formLogin(login -> login // Configuration du login via un formulaire
+                        .loginPage("/login")
+                        .successHandler(loginSuccessHandler) // Redirection vers l'accueil après authentification
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // URL de déconnexion
+                        .logoutSuccessUrl("/") // Redirige vers la page de base
+                        .invalidateHttpSession(true) // Ferme la session
+                        .deleteCookies("JSESSIONID") // Suppression des cookies de session
+                )
+                .headers(headers -> headers.frameOptions((frameOptions) -> frameOptions.disable())); // Peut être nécessaire pour afficher correctement H2-Console
+
+        // Gestion de la session utilisateur
+        http.sessionManagement(session -> session
+                .invalidSessionUrl("/login?expired=true") // Redirige à la page de connexion si l'utilisateur tente d'accéder à une page protégée avec une session invalide
+                .maximumSessions(1) // Limitation de une session par utilisateur
+                .expiredUrl("/login?session-expired=true") // Redirige à la page dee connexion quand la session est expirée (timeout/logout)
+        );
+
+        http.exceptionHandling(exceptionHandling -> exceptionHandling
+                .accessDeniedPage("/error") // Redirige les autres erreurs et accès aux pages protégées sans permissions à une page d'erreur
+        );
+
+        return http.build();
+
     }
 
     /**
