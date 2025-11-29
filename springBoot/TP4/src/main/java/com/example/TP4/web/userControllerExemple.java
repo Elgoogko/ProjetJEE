00a79dcb73437b3f5b288c268f69d33774688cc8
Alouhette <evan.lippens@gmail.com>
2025-11-29 16:08:46 +0100

    fixed imports (the code is the one from TP4 for the test project)

--- springBoot/TP4/src/main/java/com/example/TP4/web/userControllerExemple.java
+++ springBoot/TP4/src/main/java/com/example/TP4/web/userControllerExemple.java
@@ -1,10 +1,10 @@
 package TP4.src.main.java.com.example.TP4.web;
 
+import TP4.src.main.java.com.example.TP4.userService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.TP4.userService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
